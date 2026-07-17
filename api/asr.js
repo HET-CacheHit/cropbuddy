f50ee9648ff977e577341cc33602211c8b5b61bb
HET-CacheHit <hetp32172@gmail.com>
2026-07-17 09:53:14 +0530

    Enable CORS headers and handle preflight OPTIONS requests in predict_custom and asr endpoints

--- api/asr.js
+++ api/asr.js
@@ -1,6 +1,20 @@
 require('dotenv').config();
 
 module.exports = async function (req, res) {
+    // CORS Headers Setup
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
     try {
         const { audio } = req.body;
         if (!audio) {
--- api/predict_custom.js
+++ api/predict_custom.js
@@ -3,6 +3,20 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = async (req, res) => {
+    // CORS Headers Setup
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
     try {
         const { image } = req.body;
         if (!image) {
